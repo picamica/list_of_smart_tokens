999cb2ebc305c181999af379ddc2589bef21f0bd
picamica <kasparastomas1@gmail.com>
2022-07-20 20:30:16 +0300

    created models

---
+++ listOfTokens/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import bscToken, network
+
+admin.site.register(bscToken)
+admin.site.register(network)
+
+# Register your models here.
---
+++ listOfTokens/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+
+class general(models.Model):
+  created_on = models.DateTimeField(auto_now_add=True)
+  scannerLink = models.URLField('name', max_length=128)
+  exchangerLink = models.URLField('exchanger', max_length=128)
+
+  class Meta:
+    abstract = True
+    ordering = ['scannerLink', 'exchangerLink', 'created_on']
+
+  def __str__(self):
+    return f'{self.scannerLink} | {self.exchangerLink} | {self.created_on}'
+
+class network(models.Model):
+  networkName = models.CharField(max_length=15)
+
+  def __str__(self):
+    return self.networkName
+
+class bscToken(general):
+  name = models.CharField('Token name', max_length=50)
+  symbol = models.CharField('Token symbol', max_length=20)
+  address = models.CharField('Token address', max_length=45)
+  networkName = models.ForeignKey('network', on_delete=models.CASCADE)
+
+  def __str__(self):
+    return f'{self.name} | {general.created_on}'
+
+# Create your models here.

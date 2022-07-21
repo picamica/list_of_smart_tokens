6ec83d7e28ca3288f8ec8d6bd03863d2c9255728
picamica <kasparastomas1@gmail.com>
2022-07-21 17:37:14 +0300

    connected index.html

--- listOfTokens/views.py
+++ listOfTokens/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import bscToken, network
 
+
+def index(request):
+
+  num_tokens = bscToken.objects.all().count()
+  num_networks = network.objects.all()
+
+  context = {
+    'num_tokens': num_tokens,
+    'num_networks': num_networks,
+  }
+
+  return render(request, 'index.html', context=context)
 # Create your views here.

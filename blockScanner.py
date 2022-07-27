ece5fdfaed4988e7400f28998d288c9740d215db
picamica <kasparastomas1@gmail.com>
2022-07-27 21:16:51 +0300

    integrated sqlalchemy to scanner

--- blockScanner.py
+++ blockScanner.py
@@ -2,61 +2,115 @@ from web3 import Web3
 import json
 import time
 from web3.middleware import geth_poa_middleware
+import datetime
+from sqlalchemy import Column, Integer, String, DateTime, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
-
+engine = create_engine('sqlite:///db.sqlite3')
+Base = declarative_base(engine)
 
 w3 = Web3(Web3.HTTPProvider('https://bsc-dataseed.binance.org/'))
 w3.middleware_onion.inject(geth_poa_middleware, layer=0)
 
-with open('./panABI.json') as file:
-  panABI = json.load(file)
+class Tokens(Base):
 
-methodIds = ['0x60806040',
-'0x60a06040',
-'0x69d3c21b',
-'0x6818ce40',
-'0x60e06040',
-'0x60c06040',
-'0x61010060',
-'0x61016060',
-'0x210f5dda',
-'0x662386f2']
+  __tablename__ = 'listOfTokens_bsctoken'
 
-bscScanLink = 'https://www.bscscan.com/address/'
-pooCoinLink = 'https://poocoin.app/tokens/'
+  id = Column(Integer, primary_key = True)
+  created_on = Column(DateTime, default=datetime.datetime.utcnow)
+  scannerLink = Column(String)
+  exchangerLink = Column(String)
+  name = Column(String)
+  symbol = Column(String)
+  address = Column(String)
+  networkName_id = Column(Integer)
+
+  def __init__(self, scannerLink, exchangerLink, name, symbol, address, networkName_id):
+
+    self.scannerLink = scannerLink
+    self.exchangerLink = exchangerLink
+    self.name = name
+    self.symbol = symbol
+    self.address = address
+    self.networkName_id = networkName_id
 
 
-#Scans method id's in the latest block and returns a list of transaction hashes
-def scan():
-  hashes = []
-  pendingBlock = w3.eth.getBlock('pending', full_transactions = True)
-  for i in pendingBlock['transactions']:
-    if i.input[0:10] in methodIds:
-      hashes.append(w3.toHex(i.hash))
-    else:
+def loadSession():
+  metadata = Base.metadata
+  Session = sessionmaker(bind=engine)
+  session = Session()
+  return session
+
+if __name__ == "__main__":
+  bscScanLink = 'https://www.bscscan.com/address/'
+  pooCoinLink = 'https://poocoin.app/tokens/'
+
+  methodIds = ['0x60806040',
+  '0x60a06040',
+  '0x69d3c21b',
+  '0x6818ce40',
+  '0x60e06040',
+  '0x60c06040',
+  '0x61010060',
+  '0x61016060',
+  '0x210f5dda',
+  '0x662386f2']
+
+  with open('./panABI.json') as file:
+    panABI = json.load(file)
+
+  #Scans method id's in the latest block and returns a list of transaction hashes
+  def scan():
+    hashes = []
+    pendingBlock = w3.eth.getBlock('pending', full_transactions = True)
+    for i in pendingBlock['transactions']:
+      if i.input[0:10] in methodIds:
+        hashes.append(w3.toHex(i.hash))
+      else:
+        continue
+    return hashes
+
+  def checkTx():
+    hashes = scan()
+    infoList = []
+    for i in hashes:
+      tx = w3.eth.get_transaction_receipt(i)
+      contract = w3.eth.contract(address = tx.contractAddress, abi = panABI)
+      try:
+        infoList.extend([bscScanLink+tx.contractAddress, pooCoinLink+tx.contractAddress, contract.functions.name().call(), contract.functions.symbol().call(), tx.contractAddress])
+        return infoList
+      except Exception as e:
+        print(e)
+        continue
+    # return infoList
+
+  while True:
+    check = checkTx()
+    if check == None:
+      time.sleep(3)
       continue
-  return hashes
+    else:
+      token = Tokens(check[0], check[1], check[2], check[3], check[4], 1)
+      session = loadSession()
+      session.add(token)
+      session.commit()
+      time.sleep(3)
 
 
-def checkTx():
-  hashes = scan()
-  for i in hashes:
-    tx = w3.eth.get_transaction_receipt(i)
-    contract = w3.eth.contract(address = tx.contractAddress, abi = panABI)
-    try:
-      print(contract.functions.name().call())
-      print(contract.functions.symbol().call())
-      print(tx.contractAddress)
-      print(bscScanLink+tx.contractAddress)
-      print(pooCoinLink+tx.contractAddress)
-    except Exception as e:
-      print(e)
-      print(tx.contractAddress)
 
 
-while True:
-  checkTx()
-  time.sleep(3)
+
+
+
+
+
+
+
+
+
+
+
 
 
 

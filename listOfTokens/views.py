b03c4912f80cae5daf6fd2778e214302d8050828
picamica <kasparastomas1@gmail.com>
2022-08-08 18:24:46 +0300

    minor changes in views.py working progress on front-end

--- listOfTokens/views.py
+++ listOfTokens/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from .models import bscToken, network
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 
 def index(request):
 
@@ -18,22 +18,25 @@ def index(request):
 
 def bsc_tokens_view(request):
   time_threshold = datetime.now() - timedelta(hours=24)
-  bscTokens = bscToken.objects.all().filter(networkName=1).filter(created_on__gte=time_threshold)
+  bscTokens = bscToken.objects.all().filter(networkName=1).filter(created_on__gte=time_threshold).order_by('-created_on')
   tokensList = []
   for i in bscTokens:
+    time_diff = datetime.now(timezone.utc) - i.created_on
     item = {
       'name': i.name,
       'symbol': i.symbol,
       'address': i.address,
       'Scanner': i.scannerLink,
       'Exchange': i.exchangerLink,
-      'created': i.created_on
+      'created': i.created_on,
+      'age' : time_diff.total_seconds()
     }
     tokensList.append(item)
   return JsonResponse({'tokens': tokensList})
 
 def eth_tokens_view(request):
-  ethTokens = bscToken.objects.all().filter(networkName=2)
+  time_threshold = datetime.now() - timedelta(hours=24)
+  ethTokens = bscToken.objects.all().filter(networkName=2).filter(created_on__gte=time_threshold).order_by('-created_on')
   tokensList = []
   for i in ethTokens:
     item = {
@@ -47,7 +50,8 @@ def eth_tokens_view(request):
   return JsonResponse({'tokens': tokensList})
 
 def matic_tokens_view(request):
-  maticTokens = bscToken.objects.all().filter(networkName=3)
+  time_threshold = datetime.now() - timedelta(hours=24)
+  maticTokens = bscToken.objects.all().filter(networkName=3).filter(created_on__gte=time_threshold).order_by('-created_on')
   tokensList = []
   for i in maticTokens:
     item = {
@@ -61,7 +65,9 @@ def matic_tokens_view(request):
   return JsonResponse({'tokens': tokensList})
 
 def ftm_tokens_view(request):
-  ftmTokens = bscToken.objects.all().filter(networkName=4)
+  time_threshold = datetime.now() - timedelta(hours=24)
+  ftmTokens = bscToken.objects.all().filter(networkName=4).filter(created_on__gte=time_threshold).order_by('-created_on')
+  tokensList = []
   tokensList = []
   for i in ftmTokens:
     item = {
--- static/js/main.js
+++ static/js/main.js
@@ -1,27 +1,63 @@
+body = document.body;
+body.onload = showTokens('bsc', 'BSCscan', 'POOCOIN');
+
+const bscdiv = document.getElementById('bscdiv');
+const ethdiv = document.getElementById('ethdiv');
+const maticdiv = document.getElementById('maticdiv');
+const ftmdiv = document.getElementById('ftmdiv');
 
 
+bscdiv.addEventListener('click', function() {
+  showTokens('bsc', 'BSCscan', 'POOCOIN');
+});
 
-$.ajax({
-  type:'GET',
-  url: '/data/bsc/',
-  success: function(response){
-    console.log(response.tokens);
-    const data = response.tokens;
-    const table = document.getElementById('list-of-tokens');
-    data.forEach(el => {
-      table.innerHTML += `
-        <tr>
-          <td>${el.name}/${el.symbol}</td>
-          <td>${el.address}</td>
-          <td>
-            <a href="${el.scannerLink}">BSC</a>
-            <a href="${el.exchangerLink}">POOCOIN</a>
-          </td>
-        </tr>
-      `;
-    });
-  },
-  error: function(error){
-    console.log(error);
+ethdiv.addEventListener('click', function() {
+  showTokens('eth', 'EtherScan', 'Uniswap');
+});
+
+maticdiv.addEventListener('click', function() {
+  showTokens('matic', 'PolygonScan', 'QuickSwap');
+});
+
+ftmdiv.addEventListener('click', function() {
+  showTokens('ftm', 'FtmScan', 'SpookySwap');
+});
+
+function showTokens(endpoint, scanner, exchanger) {
+  const table = document.getElementById('list-of-tokens');
+  table.replaceChildren();
+
+  $.ajax({
+    type:'GET',
+    url: `/data/${endpoint}/`,
+    success: function(response){
+      console.log(response.tokens);
+      const data = response.tokens;
+      data.forEach(el => {
+        const date = new Date(el.age);
+        table.innerHTML += `
+          <tr>
+            <td>${el.name}/${el.symbol}</td>
+            <td>${el.address}</td>
+            <td>
+              <a href=${el.Scanner}>${scanner}</a>
+              <a href=${el.Exchange}>${exchanger}</a>
+            </td>
+            <td>${moment(el.created).fromNow()}</td>
+          </tr>
+        `;
+      });
+    },
+    error: function(error){
+      console.log(error);
   }
 });
+}
+
+
+
+
+
+
+
+
